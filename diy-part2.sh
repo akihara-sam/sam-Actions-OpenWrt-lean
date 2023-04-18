a0370122668b96b53d85bcacbd3eb7c6e0e6b65c
akihara-sam <92433496+akihara-sam@users.noreply.github.com>
2023-04-18 18:57:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.202/g' package/base-files/files/bin/config_generate
+
+# 加入编译者信息
+sed -i "s/OpenWrt /compiled by Akihara-Sam build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
