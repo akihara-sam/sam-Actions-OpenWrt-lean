59c240683286079ac3b0ca1668a13ed218299896
akihara-sam <92433496+akihara-sam@users.noreply.github.com>
2023-04-19 11:37:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.50.202/g' package/base-files/files/bin/config_generate
 
 # 加入编译者信息
-sed -i "s/OpenWrt /compiled by Akihara-Sam build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /Compiled By Akihara-Sam build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
